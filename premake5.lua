f9835b4ecc67543af5629c868a2bd2816138d447
Kamil Chowaniec <kamilchowaniec97@gmail.com>
2019-11-16 11:23:42 +0100

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "YAML"
+	location "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs {
+		"src",
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
